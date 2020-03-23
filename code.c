48805eb78c63af3f5eea4e704f2a671570d8dca2
smritisingh20 <61749510+smritisingh20@users.noreply.github.com>
2020-03-23 22:56:20 +0530

    Update code.c

--- code.c
+++ code.c
@@ -6,3 +6,16 @@ void main()
   int i,j;
   float avgwt;
   printf("enter no of processes: ");
+  for(i=0;i<n;i++)
+  {
+    printf("enter process%d name: ");
+    sacnf("%s",&p[i]);
+    printf("enter process time: ");
+    scanf("%d",&pt[i]);
+  }
+  for(i=0;i<n-1;i++)
+  {
+    for(j=i+1;j<n;j++)
+   {
+      temp1=pr[i];
+      pr[i]=pr[j];
