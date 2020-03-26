132e9f68bc36d6d4688c66656c0d6408b5b0e285
smritisingh20 <61749510+smritisingh20@users.noreply.github.com>
2020-03-26 14:47:30 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,46 +1,53 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+int main()
 {
   char p[10][5],temp[5];
-  int i,j;
+  int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
   float avgwt;
   printf("enter no of processes: ");
+  scanf("%d",&n);
   for(i=0;i<n;i++)
   {
-    printf("enter process%d name: ");
-    sacnf("%s",&p[i]);
+    printf("enter process%d name: ",i+1);
+    scanf("%s",&p[i]);
     printf("enter process time: ");
     scanf("%d",&pt[i]);
+    printf("enter prority: ");
+    scanf("%d",&pr[i]);
   }
   for(i=0;i<n-1;i++)
   {
-    for(j=i+1;j<n;j++)
-   {
+  for(j=i+1;j<n;j++)
+  {
+  if(pr[i]>pr[j])
+      {
       temp1=pr[i];
       pr[i]=pr[j];
       pr[j]=temp1;
       temp1=pt[i];
-      pt[i]=pt=[j];
+      pt[i]=pt[j];
       pt[j]=temp1;
+      strcpy(temp,p[i]);
       strcpy(p[i],p[j]);
-      strcpy(p[j],temp)
+      strcpy(p[j],temp);
     }
   }
 }
 wt[0]=0;
  for(i=1;i<n;i++)
  {
-   wt[i]=wt[i-1]+et[i-1];
+   wt[i]=wt[i-1]+wt[i-1];
    totwt=totwt+wt[i];
  }
-printf("p_name p_time priority w_time\n);
+  avgwt=(float)totwt/n;
+printf("p_name\t p_time\t priority\t w_time\n");
 for(i=0;i<n;i++)
        {
-         printf("%S %d %d %d",p[i],pt[i],pr[i],wt[i]);
+         printf("%S\t %d\t %d\t %d\n",p[i],pt[i],pr[i],wt[i]);
        }
-       printf("total waiting time=%d",tot);
-       getch();
+       printf("total waiting time=%d\n avg waiting time=%f",totwt,avgwt);
+      
        }
 
 
